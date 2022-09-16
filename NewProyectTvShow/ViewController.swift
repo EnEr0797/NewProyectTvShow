0ff00b399b30d61261d1029ab61b55e0c61bc525
Luis Enrique <luis.enrique.erazo.tescha@gmail.com>
2022-09-16 00:36:44 -0500

    Initial Commit

---
+++ NewProyectTvShow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewProyectTvShow
+//
+//  Created by Luis Enrique Erazo Amador on 16/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
