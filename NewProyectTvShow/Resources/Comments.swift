08ab0a6d572ca7006671b1f07ea01f3fac7684c9
Luis Enrique <luis.enrique.erazo.tescha@gmail.com>
2022-09-20 09:41:02 -0500

    Add comments

---
+++ NewProyectTvShow/Resources/Comments.swift
@@ -0,0 +1,56 @@
+//
+//  Comments.swift
+//  NewProyectTvShow
+//
+//  Created by Luis Enrique Erazo Amador on 20/09/22.
+//
+
+/*
+ Hola!
+ Este archivo tiene como intención añadir las preguntas finales del PDF compartido
+ 
+ ************ PREGUNTA N°1************************
+ 
+ Explicar la arquitectura que utilizaste en tu proyecto y la razón por la que
+ decidiste utilizarla
+ 
+ R= En mi caso traté de utilizar la arquitectura MVVC ya que es una de las más utilizadas y conocidas en la industria.
+    Esta arquitectura se puede dividir en 3 partes o más, las cuales son: VISTAS, CONTROLADORES (FUNCIONALIDAD) Y
+    MODELOS (CLASES O ESTRUCTURAS) y en mi caso agregué un segmento aparte con la única intención de poder organizar mejor el código.
+ 
+    
+ ************ PREGUNTA N°2************************
+ Lista de las librerías externas que utilizaste, explicándo qué hacen y la razón
+ por la que la elegiste
+ 
+ R= En lo personal traté de no utilizar ninguna librería externa, sino utilizar los componentes nativos de Swift para poder jugar con ellos y ver como es que funcionan.
+     Lo único externo que agregué al proyecto es la tipografía Barlow ya que en lo personal se me hace una tipografía elegante y con gran recomendación UI
+ 
+ ************ PREGUNTA N°3************************
+
+ Qué parte(s) de tu código pueden ser mejoradas si tuvieras más tiempo?
+ 
+ 
+    En lo personal a mi me gustaría poder aplicar las mejores practicas en este proyecto respecto a la segmentación del código, una mejor arquitectura y sobre todo unas variables y constantes más entendibles y modulares para que un desarrollador externo pueda tener mejor claridad al momento de leer el código
+ 
+ ************ PREGUNTA N°4************************
+
+ Cuáles fueron los mayores problemas que encontraste en el desarrollo de
+ la práctica y cómo los resolviste?
+ 
+ 
+ En lo personal a mi me costó un poco el consumo de la API ya que me la pasé mucho tiempo tratando de consumirla y al momento de querer mapearla no me arrojaba nada, después me pude dar cuenta que mi estructura no estaba correcta y al momento de corregirla ya me arrojó la información que necesitaba, pero también me costó trabajo poder mapear la imagen, traté de pintarlal, pero por cuestión de tiempo ya no lo pude finalizarla
+ 
+ 
+ ************ PREGUNTA N°5************************
+
+ 
+ Si alguno de los puntos solicitados no los concluiste, menciona por qué. Si
+ fuera por cuestión de tiempo, trata de mencionar qué estrategia llevarías a cabo para desarrollarlo
+ 
+ 
+ Me faltó concluir el mapeo de la imagen y las ultimas alertas, eso no lo pude concluir por cuestion de tiempo. Lo que yo podría hacer para poder resolver esta problematica es poder organizar mis tiempos para darle a los puntos más complicados y asi poder entregar este proyecto en tiempo y forma.
+ 
+ 
+ 
+ */
